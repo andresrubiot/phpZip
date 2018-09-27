44fafa9e98d50b62d6a9d37cb6e3761fdfc20ab1
ANDRES RUBIO <andresrubiot5@gmail.com>
2018-09-27 00:13:01 -0500

    Crear .Zip con PHP
    Creacion de un archivo ZIP mediante la libreria nativa de PHP

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$zip = new ZipArchive();
+//Nombre del archivo ZIP que se creara
+$archivo = "prueba.zip";
+
+if($zip->open($archivo,ZIPARCHIVE::CREATE)==true){
+  //Nombre y ruta del archivo que se agregara al zip
+  $zip->addfile("archivo1.txt");
+  //Nombre y ruta del archivo que se agregara al zip
+  $zip->addfile("archivo2.txt");
+  //Nombre y ruta del archivo que se agregara al zip
+  $zip->addfile("prueba/archivo1.txt");
+  $zip->close();
+  echo "Se ha creado el archivo ".$archivo;
+}else{
+  echo "Se ha presentado un error al crear el archivo ".$archivo;
+}
+
+/*
+NOTA
+
+Verificar que contemos con permisos de escritura dentro de la carpeta que se creara el archivo
+*/
+
+?>
